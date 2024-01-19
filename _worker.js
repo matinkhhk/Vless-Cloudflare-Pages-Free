abb9d0b64137f281a4a8174cab087a337a4e117a
matinkhhk <63220875+matinkhhk@users.noreply.github.com>
2024-01-19 19:06:29 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 // @armanibash
 import { connect } from 'cloudflare:sockets';
 
-let userID = 'ebe6d7db-5d6b-4b86-85e2-cd7b2ebf26c6';
+let userID = 'efc1ffff-ca64-43af-ad0d-1c451dc70c13';
 
 const proxyIPs = ["[2001:67c:2b0:db32:0:1:681a:404]"]; // ['cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
